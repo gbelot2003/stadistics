3f764b5a48db618fc85b46b628666e5e43d29e04
gbelot2003 <gbelot2003@hotmail.com>
2016-03-02 11:36:20 -0600

    agregando images a lienzo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,16 +7,36 @@
     <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
+
 <header>
     <nav class="navbar navbar-inverse">
         <menu-nav></menu-nav>
     </nav>
 </header>
-<main class="container">
-    <div class="row">
-        <div class="nga-default nga-stagger nga-slide-down col-md-12" ng-view></div>
-    </div>
-</main>
+    <main class="container">
+        <div class="row">
+            <div class="nga-default nga-stagger nga-slide-down col-md-12" ng-view></div>
+        </div>
+    </main>
+    <footer>
+        <hr />
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-responsive" src="{{ asset('images/ifex_logo_white_345x200.jpg') }}" alt="ifex">
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="{{ asset('images/clibre_logo.gif') }}" alt="clibre">
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="{{ asset('images/conexihon.jpg') }}" alt="conexihon">
+                </div>
+                <div class="col-md-3">
+                    <img class="img-responsive" src="{{ asset('images/observador.jpg') }}" alt="conexihon">
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
